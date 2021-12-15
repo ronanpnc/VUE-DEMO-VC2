79a2bd68fd43aef38e84739931fa538d6b4b30e2
ronanpnc <ronan.ogor@passerellesnumeriques.org>
2021-12-15 20:26:53 +0700

    updated from vie

--- src/components/views/FormsInputsView.vue
+++ src/components/views/FormsInputsView.vue
@@ -1,12 +1,23 @@
 <template>
   <v-row class="pa-6">
     <v-col md="12">
-      <v-row no-gutters>
-        <v-col md="6">
-          <p>XXXX</p>
+      <v-alert dense :type="alertType">
+        I'm a dense alert with a <strong>{{ alertType }}</strong> of info
+      </v-alert>
+
+      <v-row>
+        <v-col md="12">
+          <v-file-input truncate-length="15"></v-file-input>
         </v-col>
         <v-col md="6">
-          <p>XXXX</p>
+          <v-combobox
+            v-model="select"
+            :items="items"
+            label="Combobox"
+            multiple
+            outlined
+            dense
+          ></v-combobox>
         </v-col>
       </v-row>
     </v-col>
